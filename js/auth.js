28b235e4ad324437b027b4dc7affe6e77abb7dca
Gefferson Casasola <gcasasolah@uni.pe>
2022-12-26 21:35:28 -0500

    creacion de login y la vista de usuarios

---
+++ js/auth.js
@@ -0,0 +1,52 @@
+var BASE_URL="http://127.0.0.1:8000/"
+
+function validateAuth(archivoRedirect) {
+  let token = localStorage.getItem("authTokens");
+  if (!token) {
+    window.location.href = archivoRedirect;
+  }
+}
+
+function validateToken(archivoRedirect) {
+  let token = localStorage.getItem("authTokens");
+  if (token) {
+    window.location.href = archivoRedirect;
+  }
+}
+
+function updateTokenInterval() {
+    let fourMinutes=1000*60*25 //25 minutes
+    let interval = setInterval(() => {
+        updateToken();
+      }, fourMinutes);
+      return () => clearInterval(interval);
+}
+
+let updateToken = async () => {
+  console.log("UPDATE TOKEN CALLED");
+  let authTokens = JSON.parse(localStorage.getItem("authTokens"));
+
+  let response = await fetch(BASE_URL+"users/jwt/refresh/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ refresh: authTokens?.refresh }),
+  });
+  let data = await response.json();
+  if (response.status === 200) {
+    authTokens.access=data.access
+    localStorage.setItem("authTokens", JSON.stringify(authTokens));
+  } else {
+    logoutUser();
+  }
+
+};
+
+let logoutUser = () => {
+  localStorage.removeItem("authTokens");
+  localStorage.removeItem("user");
+  window.location.replace("./login.html");
+};
+
+export { validateAuth, validateToken, updateTokenInterval,logoutUser, BASE_URL };
